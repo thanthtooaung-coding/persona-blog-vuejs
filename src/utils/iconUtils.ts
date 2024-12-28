cb3ae74d9a480d63be08e3de89a3eacad9abae5e
thanthtooaung-coding <thanthtoo1285@gmail.com>
2024-12-28 10:54:19 +0630

    Fix iconName type

--- src/utils/iconUtils.ts
+++ src/utils/iconUtils.ts
@@ -1,6 +1,6 @@
 import * as LucideIcons from 'lucide-vue-next'
 
-export const getIconComponent = (iconName: string) => {
+export const getIconComponent = (iconName: any) => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return (LucideIcons as any)[iconName] || LucideIcons.HelpCircle
 }
